77682fd0959540e2c7207f04893c3daeb80b8a75
Martha <marthasibiya25@gmail.com>
2026-01-17 17:20:28 +0200

    save all changes

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Populate current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Populate last modified date
+document.getElementById("lastModified").textContent = document.lastModified;
