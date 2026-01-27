fa934c36a7bbc15280947c5eaf999751e60b2af8
Martha <marthasibiya25@gmail.com>
2026-01-27 19:07:21 +0200

    save all changes

--- week03/script.js
+++ week03/script.js
@@ -1,20 +1,5 @@
-const temperature = 8;
-const windSpeed = 10;
-
-function calculateWindChill(temp, speed) {
-    if (temp <= 10 && speed > 4.8) {
-        return Math.round(
-            13.12 + 0.6215 * temp -
-            11.37 * Math.pow(speed, 0.16) +
-            0.3965 * temp * Math.pow(speed, 0.16)
-        );
-    }
-    return "N/A";
-}
-
+// set current year
 document.getElementById("year").textContent = new Date().getFullYear();
+
+// set last modified date
 document.getElementById("lastModified").textContent = document.lastModified;
-
-const chill = calculateWindChill(temperature, windSpeed);
-document.getElementById("windChill").textContent = chill;
-
